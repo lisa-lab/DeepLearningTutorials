4921e472289d29f01295ba16638f1e5cef4fd92c
Guillaume Desjardins <guillaume.desjardins@gmail.com>
2010-02-17 01:39:12 -0500

    new version of RBM code: implements PCD instead of CD, used to generate good samples and filters.

--- code/rbm.py
+++ code/rbm.py
@@ -210,21 +210,6 @@ class RBM(object):
         self.params     = [self.W, self.hbias, self.vbias]
         self.batch_size = self.input.shape[0]
 
-
-    def gibbs_1(self, v0_sample):
-        ''' This function implements one step of Gibbs sampling '''
-        # compute the activation of the hidden units given a sample of the
-        # vissibles
-        h0_mean = T.nnet.sigmoid(T.dot(v0_sample, self.W) + self.hbias)
-        # get a sample of the hiddens given their activation
-        h0_sample = self.theano_rng.binomial(size = h0_mean.shape, n = 1, prob = h0_mean)
-        # compute the activation of the visible given the hidden sample
-        v1_mean = T.nnet.sigmoid(T.dot(h0_sample, self.W.T) + self.vbias)
-        # get a sample of the visible given their activation
-        v1_sample = self.theano_rng.binomial(size = v1_mean.shape,n = 1,prob = v1_mean)
-        return [v1_mean, v1_sample]
- 
- 
     def free_energy(self, v_sample):
         ''' Function to compute the free energy '''
         wx_b = T.dot(v_sample, self.W) + self.hbias
@@ -232,58 +217,71 @@ class RBM(object):
         hidden_term = T.sum(T.log(1+T.exp(wx_b)))
         return -hidden_term - vbias_term
 
+    def sample_h_given_v(self, v0_sample):
+        ''' This function infers state of hidden units given visible units '''
+        # compute the activation of the hidden units given a sample of the visibles
+        h1_mean = T.nnet.sigmoid(T.dot(v0_sample, self.W) + self.hbias)
+        # get a sample of the hiddens given their activation
+        h1_sample = self.theano_rng.binomial(size = h1_mean.shape, n = 1, prob = h1_mean)
+        return [h1_mean, h1_sample]
+
+    def sample_v_given_h(self, h0_sample):
+        ''' This function infers state of visible units given hidden units '''
+        # compute the activation of the visible given the hidden sample
+        v1_mean = T.nnet.sigmoid(T.dot(h0_sample, self.W.T) + self.vbias)
+        # get a sample of the visible given their activation
+        v1_sample = self.theano_rng.binomial(size = v1_mean.shape,n = 1,prob = v1_mean)
+        return [v1_mean, v1_sample]
+
+    def gibbs_hvh(self, h0_sample):
+        ''' This function implements one step of Gibbs sampling, 
+            starting from the hidden state'''
+        v1_mean, v1_sample = self.sample_v_given_h(h0_sample)
+        h1_mean, h1_sample = self.sample_h_given_v(v1_sample)
+        return [v1_mean, v1_sample, h1_mean, h1_sample]
+ 
+    def gibbs_vhv(self, v0_sample):
+        ''' This function implements one step of Gibbs sampling, 
+            starting from the visible state'''
+        h1_mean, h1_sample = self.sample_h_given_v(v0_sample)
+        v1_mean, v1_sample = self.sample_v_given_h(h1_sample)
+        return [h1_mean, h1_sample, v1_mean, v1_sample]
+ 
     def cd(self, lr = 0.1, persistent=None):
-        """ This functions implements one step of CD-1
+        """ 
+        This functions implements one step of CD-1 or PCD-1
 
-        :param lr: learning rate used to train the RBM
+        :param lr: learning rate used to train the RBM 
+        :param persistent: None for CD. For PCD, shared variable containing old state
+        of Gibbs chain. This must be a shared variable of size (batch size, number of
+        hidden units).
 
-        Return a tuple of values describing a CD step. The first value 
-        in the tuple is the reconstruction cross-entropy cost, a proxy 
-        to what CD tries to minimize, while the second is an update 
-        dictionary. The dictionary contains the update rules for weights
-        and biases but also an update of the shared variable used to store
-        the persistent chain, if one is used.
-        
-        Note that the reconstruction cross-entropy cost is just
-        a proxy of what CD actually tries to minimize, do not confuse the 
-        two.
-
-        If persistent is None, it defaults to self.input
-
-        CD aka CD1 - cd()
-        PCD        - cd(persistent=shared(numpy.asarray(initializer)))
+        Returns the updates dictionary. The dictionary contains the update rules for weights
+        and biases but also an update of the shared variable used to store the persistent
+        chain, if one is used.
         """
 
+        # compute positive phase
+        ph_mean, ph_sample = self.sample_h_given_v(self.input)
+
+        # decide how to initialize persistent chain:
+        # for CD, we use the newly generate hidden sample
+        # for PCD, we initialize from the old state of the chain
         if persistent is None:
-            chain_start = self.input
+            chain_start = ph_sample
         else:
             chain_start = persistent
 
+        # perform actual negative phase
+        [nv_mean, nv_sample, nh_mean, nh_sample] = self.gibbs_hvh(chain_start)
 
-        # define the graph for positive phase
-        ph_act    = T.dot(chain_start, self.W) + self.hbias
-        ph_mean   = T.nnet.sigmoid(ph_act)
-        ph_sample = self.theano_rng.binomial(size = ph_mean.shape, n = 1,
-                                                         prob = ph_mean)
-
-        # define the graph for the negative phase
-        nv_act    = T.dot(ph_sample, self.W.T) + self.vbias
-        nv_mean   = T.nnet.sigmoid(nv_act)
-        nv_sample = self.theano_rng.binomial(size = nv_mean.shape, n = 1,
-                                                         prob = nv_mean)
-        nh_act    = T.dot(nv_sample, self.W) + self.hbias
-        nh_mean   = T.nnet.sigmoid(nh_act)
-        nh_sample = self.theano_rng.binomial(size = nh_mean.shape, n = 1,
-                                                         prob = nh_mean)
-
+        # determine gradients on RBM parameters
         g_vbias = T.sum( self.input - nv_mean, axis = 0)/self.batch_size
         g_hbias = T.sum( ph_mean    - nh_mean, axis = 0)/self.batch_size
         g_W = T.dot(ph_mean.T, self.input   )/ self.batch_size - \
               T.dot(nh_mean.T, nv_mean      )/ self.batch_size
 
         gparams = [g_W.T, g_hbias, g_vbias]
-        cross_entropy = T.mean(T.sum( self.input*T.log(nv_mean) 
-                       + (1 - self.input)*T.log(1-nv_mean), axis = 1))
 
         # constructs the update dictionary
         updates = {}
@@ -292,10 +290,10 @@ class RBM(object):
 
         if persistent:
             # Note that this works only if persistent is a shared variable
-            updates[persistent] = nv_sample
+            updates[persistent] = T.cast(nh_sample, dtype=theano.config.floatX)
 
 
-        return (cross_entropy, updates)
+        return updates
 
 
 def test_rbm( learning_rate=0.1, training_epochs = 15, \
@@ -337,32 +335,45 @@ def test_rbm( learning_rate=0.1, training_epochs = 15, \
     index = T.lscalar()    # index to a [mini]batch 
     x     = T.matrix('x')  # the data is presented as rasterized images
 
-
     rng        = numpy.random.RandomState(123)
     theano_rng = RandomStreams( rng.randint(2**30))
+
+    # initialize storage fot the persistent chain (state = hidden layer of chain)
+    persistent_chain = theano.shared(numpy.zeros((batch_size, 500)))
 
     # construct the RBM class
     rbm = RBM( input = x, n_visible=28*28, \
                n_hidden = 500,numpy_rng = rng, theano_rng = theano_rng)
 
     # get the cost and the gradient corresponding to one step of CD
-    (cross_entropy, updates) = rbm.cd()
+    updates = rbm.cd(lr=learning_rate, persistent=persistent_chain)
 
-    train_rbm = theano.function([index], cross_entropy, 
+
+    #################################
+    #     Training the RBM          #
+    #################################
+
+    # it is ok for a theano function to have no output
+    # the purpose of train_rbm is solely to update the RBM parameters
+    train_rbm = theano.function([index], [],
            updates = updates, 
            givens = { x: train_set_x[index*batch_size:(index+1)*batch_size]})
 
     plotting_time = 0.
     start_time = time.clock()  
+
     # go through training epochs 
     for epoch in xrange(training_epochs):
+
         # go through the training set
         c = []
         for batch_index in xrange(n_train_batches):
-           c += [ train_rbm(batch_index) ]
-        print 'Training epoch %d, average cross-entropy reconstruction cost '%epoch, numpy.mean(c)
+           train_rbm(batch_index)
+
+        print 'Training epoch %d '%epoch
+
+        # Plot filters after each training epoch
         plotting_start = time.clock()
-        #           Plot filters after each training epoch
         # Construct image from the weight matrix 
         image = PIL.Image.fromarray(tile_raster_images( X = rbm.W.value.T,
                  img_shape = (28,28),tile_shape = (10,10), 
@@ -370,16 +381,17 @@ def test_rbm( learning_rate=0.1, training_epochs = 15, \
         image.save('filters_at_epoch_%i.png'%epoch) 
         plotting_stop = time.clock()
         plotting_time += (plotting_stop - plotting_start)
+
     end_time = time.clock()
 
     pretraining_time = (end_time - start_time) - plotting_time
 
     print ('Training took %f minutes' %(pretraining_time/60.))
 
-   
-    # Sampling the RBM
-
-    # Initialize the persistent chain with some sample from the test 
+  
+    #################################
+    #     Sampling from the RBM     #
+    #################################
 
     # find out the number of test samples  
     number_of_test_samples = test_set_x.value.shape[0]
@@ -387,18 +399,23 @@ def test_rbm( learning_rate=0.1, training_epochs = 15, \
     # pick two initial starting points randomly 
     sample = rng.randint(number_of_test_samples-20)
 
-    # initialize 20 persistent chains in parallel over which we will loop
-    persistent_chain = theano.shared( test_set_x.value[sample:sample+20])
+    # Initialize the persistent chain with some sample from the test 
+    persistent_vis_chain = theano.shared(test_set_x.value[sample:sample+20])
+
+    # define one step of Gibbs sampling (mf = mean-field)
+    [hid_mf, hid_sample, vis_mf, vis_sample] =  rbm.gibbs_vhv(persistent_vis_chain)
 
     # the sample at the end of the channel is returned by ``gibbs_1`` as 
     # its second output; note that this is computed as a binomial draw, 
     # therefore it is formed of ints (0 and 1) and therefore needs to 
-    # be converted to the same dtype as ``persistent_chain``
-    chain_step_sample = T.cast(rbm.gibbs_1(persistent_chain)[1], dtype =
-                                                    theano.config.floatX)
+    # be converted to the same dtype as ``persistent_vis_chain``
+    vis_sample = T.cast(vis_sample, dtype=theano.config.floatX)
+
     # construct the function that implements our persistent chain 
-    sample_fn = theano.function([], chain_step_sample, 
-                      updates = { persistent_chain:chain_step_sample })
+    # we generate the "mean field" activations for plotting and the actual samples for
+    # reinitializing the state of our persistent chain
+    sample_fn = theano.function([], [vis_mf, vis_sample],
+                      updates = { persistent_vis_chain:vis_sample})
 
     # sample the RBM, plotting every `plot_every`-th sample; do this 
     # until you plot at least `n_samples`
@@ -408,10 +425,11 @@ def test_rbm( learning_rate=0.1, training_epochs = 15, \
     for idx in xrange(n_samples):
         # do `plot_every` intermediate samplings of which we do not care
         for jdx in  xrange(plot_every):
-            sample_fn()
+            vis_mf, vis_sample = sample_fn()
+
         # construct image
         image = PIL.Image.fromarray(tile_raster_images( 
-                                         X          = persistent_chain.value,
+                                         X          = vis_mf,
                                          img_shape  = (28,28),
                                          tile_shape = (10,10),
                                          tile_spacing = (1,1) ) )
@@ -419,4 +437,4 @@ def test_rbm( learning_rate=0.1, training_epochs = 15, \
         image.save('sample_%i_step_%i.png'%(idx,idx*jdx))
 
 if __name__ == '__main__':
-   test_rbm() 
+    test_rbm()
