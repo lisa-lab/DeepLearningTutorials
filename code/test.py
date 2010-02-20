148e6c840ed81cc38e4f9b2474fbe255d8dd948c
Razvan Pascanu <pascanur@simplet.iro.umontreal.ca>
2010-02-20 17:33:34 -0500

    changes to test

--- code/test.py
+++ code/test.py
@@ -2,14 +2,14 @@
 import convolutional_mlp, logistic_cg, logistic_sgd, mlp, SdA
 from nose.plugins.skip import SkipTest
 #TODO: dbn, rbm, SdA, SdA_loops, convolutional_mlp
-#def test_logistic_sgd():
-#    logistic_sgd.sgd_optimization_mnist(n_epochs=10)
-#def test_logistic_cg():
-#    logistic_cg.cg_optimization_mnist(n_epochs=10)
-#def test_mlp():
-#    mlp.test_mlp(n_epochs=5)
-#def test_convolutional_mlp():
-#    convolutional_mlp.evaluate_lenet5(n_epochs=5,nkerns=[5,5])
+def test_logistic_sgd():
+    logistic_sgd.sgd_optimization_mnist(n_epochs=10)
+def test_logistic_cg():
+    logistic_cg.cg_optimization_mnist(n_epochs=10)
+def test_mlp():
+    mlp.test_mlp(n_epochs=5)
+def test_convolutional_mlp():
+    convolutional_mlp.evaluate_lenet5(n_epochs=5,nkerns=[5,5])
 def test_dbn():
     raise SkipTest('Implementation not finished')
 def test_rbm():
