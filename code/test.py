170eee0dfa06c34f4a881de535f701a2b47d862f
Frédéric Bastien <nouiz@nouiz.org>
2011-07-23 17:26:30 -0400

    Don't consider not executed code for nan.

--- code/test.py
+++ code/test.py
@@ -53,16 +53,20 @@ def speed():
     do_float32=True
     do_gpu=True
 
+    algo_executed = [s for idx,s in enumerate(algo) if to_exec[idx]]
+
     expected_times_64=numpy.asarray([  12.42313051,   28.09523582,  106.35365391,  116.79225969,  153.12310314,  
                                        425.09175086,  642.72824597,  652.52828193])
     expected_times_32=numpy.asarray([  13.29699826,   32.42813158,   68.03559947,  105.54640913,  107.00527334,
                                        242.41721797,  490.40798998, 528.88854146])
     expected_times_gpu=numpy.asarray([   3.07663488,  7.55523491,   18.99226785,   9.58915591,   24.13007045,
                                          24.77524018,  92.66246653,  322.34032917])
+    expected_times_64 = [s for idx,s in enumerate(expected_times_64) if to_exec[idx]]
+    expected_times_32 = [s for idx,s in enumerate(expected_times_32) if to_exec[idx]]
+    expected_times_gpu = [s for idx,s in enumerate(expected_times_gpu) if to_exec[idx]]
 
     def time_test(m,l,idx,f,**kwargs):
         if not to_exec[idx]:
-            l[idx]=numpy.nan
             return
         print algo[idx]
         ts=m.call_time
@@ -73,11 +77,11 @@ def speed():
             l[idx]=numpy.nan
             return
         te=m.call_time
-        l[idx]=te-ts
+        l.append(te-ts)
 
     def do_tests():
         m=theano.compile.mode.get_default_mode()
-        l=numpy.zeros(len(algo))
+        l=[]
         time_test(m,l,0, logistic_sgd.sgd_optimization_mnist,n_epochs=30)
         time_test(m,l,1, logistic_cg.cg_optimization_mnist,n_epochs=30)
         time_test(m,l,2, mlp.test_mlp, n_epochs=5)
@@ -86,16 +90,15 @@ def speed():
         time_test(m,l,5, SdA.test_SdA, pretraining_epochs = 1, training_epochs = 2, batch_size = 300)
         time_test(m,l,6, DBN.test_DBN, pretraining_epochs = 1, training_epochs = 2, batch_size = 300)
         time_test(m,l,7, rbm.test_rbm, training_epochs = 1, batch_size = 300, n_chains = 1, n_samples = 1, output_folder =  'tmp_rbm_plots')
-        return l
+        return numpy.asarray(l)
 
 
     #test in float64 in FAST_RUN mode on the cpu
     if do_float64:
-        print >> sys.stderr, algo
         theano.config.floatX='float64'
         theano.config.mode='FAST_RUN'
-        float64_times=numpy.zeros(len(algo))
         float64_times=do_tests()
+        print >> sys.stderr, algo_executed
         print >> sys.stderr, 'float64 times',float64_times
         print >> sys.stderr, 'float64 expected',expected_times_64
         print >> sys.stderr, 'float64 % expected/get',expected_times_64/float64_times
@@ -103,8 +106,8 @@ def speed():
     #test in float32 in FAST_RUN mode on the cpu
     theano.config.floatX='float32'
     if do_float32:
-        float32_times=numpy.zeros(len(algo))
         float32_times=do_tests()
+        print >> sys.stderr, algo_executed
         print >> sys.stderr, 'float32 times',float32_times
         print >> sys.stderr, 'float32 expected',expected_times_32
         print >> sys.stderr, 'float32 % expected/get',expected_times_32/float32_times
@@ -113,7 +116,7 @@ def speed():
             print >> sys.stderr, 'float64/float32',float64_times/float32_times
             print >> sys.stderr
             print >> sys.stderr, 'Duplicate the timing to have everything in one place'
-            print >> sys.stderr, algo
+            print >> sys.stderr, algo_executed
             print >> sys.stderr, 'float64 times',float64_times
             print >> sys.stderr, 'float64 expected',expected_times_64
             print >> sys.stderr, 'float64 % expected/get',expected_times_64/float64_times
@@ -129,6 +132,7 @@ def speed():
     if do_gpu:
         theano.sandbox.cuda.use('gpu')
         gpu_times=do_tests()
+        print >> sys.stderr, algo_executed
         print >> sys.stderr, 'gpu times',gpu_times
         print >> sys.stderr, 'gpu expected',expected_times_gpu
         print >> sys.stderr, 'gpu % expected/get',expected_times_gpu/gpu_times
@@ -138,7 +142,7 @@ def speed():
         if (do_float64 + do_float32 + do_gpu) > 1:
             print >> sys.stderr
             print >> sys.stderr, 'Duplicate the timing to have everything in one place'
-            print >> sys.stderr, algo
+            print >> sys.stderr, algo_executed
             if do_float64:
                 print >> sys.stderr, 'float64 times',float64_times
                 print >> sys.stderr, 'float64 expected',expected_times_64
