59ee79ecb2ae64f61e53d15d695b6b414f18a00b
Frederic Bastien <nouiz@nouiz.org>
2012-03-20 14:20:56 -0400

    pep8

--- code/utils.py
+++ code/utils.py
@@ -10,16 +10,17 @@ image from a set of samples or weights.
 import numpy
 
 
-def scale_to_unit_interval(ndar,eps=1e-8):
+def scale_to_unit_interval(ndar, eps=1e-8):
     """ Scales all values in the ndarray ndar to be between 0 and 1 """
     ndar = ndar.copy()
     ndar -= ndar.min()
-    ndar *= 1.0 / (ndar.max()+eps)
+    ndar *= 1.0 / (ndar.max() + eps)
     return ndar
 
 
-def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
-              scale_rows_to_unit_interval = True, output_pixel_vals = True):
+def tile_raster_images(X, img_shape, tile_shape, tile_spacing=(0, 0),
+                       scale_rows_to_unit_interval=True,
+                       output_pixel_vals=True):
     """
     Transform an array with one flattened image per row, into an array in
     which images are reshaped and layed out like tiles on a floor.
@@ -70,15 +71,17 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
         assert len(X) == 4
         # Create an output numpy ndarray to store the image
         if output_pixel_vals:
-            out_array = numpy.zeros((out_shape[0], out_shape[1], 4), dtype='uint8')
+            out_array = numpy.zeros((out_shape[0], out_shape[1], 4),
+                                    dtype='uint8')
         else:
-            out_array = numpy.zeros((out_shape[0], out_shape[1], 4), dtype=X.dtype)
+            out_array = numpy.zeros((out_shape[0], out_shape[1], 4),
+                                    dtype=X.dtype)
 
         #colors default to 0, alpha defaults to 1 (opaque)
         if output_pixel_vals:
-            channel_defaults = [0,0,0,255]
+            channel_defaults = [0, 0, 0, 255]
         else:
-            channel_defaults = [0.,0.,0.,1.]
+            channel_defaults = [0., 0., 0., 1.]
 
         for i in xrange(4):
             if X[i] is None:
@@ -87,12 +90,14 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
                 dt = out_array.dtype
                 if output_pixel_vals:
                     dt = 'uint8'
-                out_array[:,:,i] = numpy.zeros(out_shape,
-                        dtype=dt)+channel_defaults[i]
+                out_array[:, :, i] = numpy.zeros(out_shape,
+                        dtype=dt) + channel_defaults[i]
             else:
                 # use a recurrent call to compute the channel and store it
                 # in the output
-                out_array[:,:,i] = tile_raster_images(X[i], img_shape, tile_shape, tile_spacing, scale_rows_to_unit_interval, output_pixel_vals)
+                out_array[:, :, i] = tile_raster_images(
+                    X[i], img_shape, tile_shape, tile_spacing,
+                    scale_rows_to_unit_interval, output_pixel_vals)
         return out_array
 
     else:
@@ -106,28 +111,25 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
             dt = 'uint8'
         out_array = numpy.zeros(out_shape, dtype=dt)
 
-
         for tile_row in xrange(tile_shape[0]):
             for tile_col in xrange(tile_shape[1]):
                 if tile_row * tile_shape[1] + tile_col < X.shape[0]:
+                    this_x = X[tile_row * tile_shape[1] + tile_col]
                     if scale_rows_to_unit_interval:
                         # if we should scale values to be between 0 and 1
                         # do this by calling the `scale_to_unit_interval`
                         # function
-                        this_img = scale_to_unit_interval(X[tile_row * tile_shape[1] + tile_col].reshape(img_shape))
+                        this_img = scale_to_unit_interval(
+                            this_x.reshape(img_shape))
                     else:
-                        this_img = X[tile_row * tile_shape[1] + tile_col].reshape(img_shape)
+                        this_img = this_x.reshape(img_shape)
                     # add the slice to the corresponding position in the
                     # output array
                     c = 1
                     if output_pixel_vals:
                         c = 255
                     out_array[
-                        tile_row * (H+Hs):tile_row*(H+Hs)+H,
-                        tile_col * (W+Ws):tile_col*(W+Ws)+W
-                        ] \
-                        = this_img * c
+                        tile_row * (H + Hs): tile_row * (H + Hs) + xsH,
+                        tile_col * (W + Ws): tile_col * (W + Ws) + W
+                        ] = this_img * c
         return out_array
-
-
-
