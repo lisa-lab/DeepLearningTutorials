56c180955dca2c92991e131d81808a06c493128a
Frederic Bastien <nouiz@nouiz.org>
2012-03-20 14:17:47 -0400

    trailing white space.

--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,8 @@
-""" This file contains different utility functions that are not connected 
-in anyway to the networks presented in the tutorials, but rather help in 
-processing the outputs into a more understandable way. 
+""" This file contains different utility functions that are not connected
+in anyway to the networks presented in the tutorials, but rather help in
+processing the outputs into a more understandable way.
 
-For example ``tile_raster_images`` helps in generating a easy to grasp 
+For example ``tile_raster_images`` helps in generating a easy to grasp
 image from a set of samples or weights.
 """
 
@@ -18,17 +18,17 @@ def scale_to_unit_interval(ndar,eps=1e-8):
     return ndar
 
 
-def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0), 
+def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
               scale_rows_to_unit_interval = True, output_pixel_vals = True):
     """
-    Transform an array with one flattened image per row, into an array in 
+    Transform an array with one flattened image per row, into an array in
     which images are reshaped and layed out like tiles on a floor.
 
-    This function is useful for visualizing datasets whose rows are images, 
-    and also columns of matrices for transforming those rows 
+    This function is useful for visualizing datasets whose rows are images,
+    and also columns of matrices for transforming those rows
     (such as the first layer of a neural net).
 
-    :type X: a 2-D ndarray or a tuple of 4 channels, elements of which can 
+    :type X: a 2-D ndarray or a tuple of 4 channels, elements of which can
     be 2-D ndarrays or None;
     :param X: a 2-D array in which every row is a flattened image.
 
@@ -37,7 +37,7 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
 
     :type tile_shape: tuple; (rows, cols)
     :param tile_shape: the number of images to tile (rows, cols)
-    
+
     :param output_pixel_vals: if output should be pixel values (i.e. int8
     values) or floats
 
@@ -45,30 +45,30 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
     being plotted to [0,1] or not
 
 
-    :returns: array suitable for viewing as an image.  
+    :returns: array suitable for viewing as an image.
     (See:`PIL.Image.fromarray`.)
     :rtype: a 2-d array with same dtype as X.
 
     """
- 
+
     assert len(img_shape) == 2
     assert len(tile_shape) == 2
     assert len(tile_spacing) == 2
 
-    # The expression below can be re-written in a more C style as 
-    # follows : 
+    # The expression below can be re-written in a more C style as
+    # follows :
     #
     # out_shape    = [0,0]
     # out_shape[0] = (img_shape[0]+tile_spacing[0])*tile_shape[0] -
     #                tile_spacing[0]
     # out_shape[1] = (img_shape[1]+tile_spacing[1])*tile_shape[1] -
     #                tile_spacing[1]
-    out_shape = [(ishp + tsp) * tshp - tsp for ishp, tshp, tsp 
+    out_shape = [(ishp + tsp) * tshp - tsp for ishp, tshp, tsp
                         in zip(img_shape, tile_shape, tile_spacing)]
 
     if isinstance(X, tuple):
         assert len(X) == 4
-        # Create an output numpy ndarray to store the image 
+        # Create an output numpy ndarray to store the image
         if output_pixel_vals:
             out_array = numpy.zeros((out_shape[0], out_shape[1], 4), dtype='uint8')
         else:
@@ -82,7 +82,7 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
 
         for i in xrange(4):
             if X[i] is None:
-                # if channel is None, fill it with zeros of the correct 
+                # if channel is None, fill it with zeros of the correct
                 # dtype
                 dt = out_array.dtype
                 if output_pixel_vals:
@@ -90,13 +90,13 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
                 out_array[:,:,i] = numpy.zeros(out_shape,
                         dtype=dt)+channel_defaults[i]
             else:
-                # use a recurrent call to compute the channel and store it 
+                # use a recurrent call to compute the channel and store it
                 # in the output
                 out_array[:,:,i] = tile_raster_images(X[i], img_shape, tile_shape, tile_spacing, scale_rows_to_unit_interval, output_pixel_vals)
         return out_array
 
     else:
-        # if we are dealing with only one channel 
+        # if we are dealing with only one channel
         H, W = img_shape
         Hs, Ws = tile_spacing
 
@@ -111,13 +111,13 @@ def tile_raster_images(X, img_shape, tile_shape,tile_spacing = (0,0),
             for tile_col in xrange(tile_shape[1]):
                 if tile_row * tile_shape[1] + tile_col < X.shape[0]:
                     if scale_rows_to_unit_interval:
-                        # if we should scale values to be between 0 and 1 
+                        # if we should scale values to be between 0 and 1
                         # do this by calling the `scale_to_unit_interval`
                         # function
                         this_img = scale_to_unit_interval(X[tile_row * tile_shape[1] + tile_col].reshape(img_shape))
                     else:
                         this_img = X[tile_row * tile_shape[1] + tile_col].reshape(img_shape)
-                    # add the slice to the corresponding position in the 
+                    # add the slice to the corresponding position in the
                     # output array
                     c = 1
                     if output_pixel_vals:
