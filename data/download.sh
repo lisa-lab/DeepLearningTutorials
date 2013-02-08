6ae67bf8f90809a78ab949755739cd2330b90231
Nicolas Boulanger-Lewandowski <nicolas_boulanger@hotmail.com>
2013-02-07 21:12:04 -0500

    license, centralized hosting and automated download for the modified Python MIDI package

--- data/download.sh
+++ data/download.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 wget http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
-wget http://www.iro.umontreal.ca/~lisa/deep/data/mnist/Nottingham.zip && unzip Nottingham.zip
+wget http://www.iro.umontreal.ca/~lisa/deep/data/Nottingham.zip && unzip Nottingham.zip
+wget http://www.iro.umontreal.ca/~lisa/deep/midi.zip && unzip midi.zip -d ../code && echo "extracted Modified Python MIDI package (GPL)"
