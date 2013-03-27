d8860cf2b62e24e879ac8298191a4e826cb5b03b
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2013-03-27 19:45:10 -0400

    Revert change that should not have been committed

--- code/convolutional_mlp.py
+++ code/convolutional_mlp.py
@@ -296,7 +296,7 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
                           ' ran for %.2fm' % ((end_time - start_time) / 60.))
 
 if __name__ == '__main__':
-    evaluate_lenet5(n_epochs=1, batch_size=1)
+    evaluate_lenet5()
 
 
 def experiment(state, channel):
