3ea654f191467a3fb4f2dbbfe52330d9803437da
Frederic <nouiz@nouiz.org>
2013-09-12 15:42:48 -0400

    Fix the unzip of midi package.

--- data/download.sh
+++ data/download.sh
@@ -2,4 +2,4 @@
 
 wget -c http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
 wget -c http://www.iro.umontreal.ca/~lisa/deep/data/Nottingham.zip && unzip -f Nottingham.zip
-wget -c http://www.iro.umontreal.ca/~lisa/deep/midi.zip && unzip -f midi.zip -d ../code && echo "extracted Modified Python MIDI package (GPL)"
+wget -c http://www.iro.umontreal.ca/~lisa/deep/midi.zip && unzip -u midi.zip -d ../code && echo "extracted Modified Python MIDI package (GPL)"
