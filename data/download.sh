cfb0a0206d6d951157538de3f341283fbe984bea
Frederic <nouiz@nouiz.org>
2013-09-13 16:19:21 -0400

    Correctly uncompress the data.

--- data/download.sh
+++ data/download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 wget -c http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
-wget -c http://www.iro.umontreal.ca/~lisa/deep/data/Nottingham.zip && unzip -f Nottingham.zip
+wget -c http://www.iro.umontreal.ca/~lisa/deep/data/Nottingham.zip && unzip -u Nottingham.zip
 wget -c http://www.iro.umontreal.ca/~lisa/deep/midi.zip && unzip -u midi.zip -d ../code && echo "extracted Modified Python MIDI package (GPL)"
