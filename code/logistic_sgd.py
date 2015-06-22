8f002a506595c9b0bacabc35b48dc085acedbc91
Mehdi Mirza <memirzamo@gmail.com>
2015-06-22 10:50:37 -0400

    add predict option to logistic regression
    mend
    
    mend

--- code/logistic_sgd.py
+++ code/logistic_sgd.py
@@ -44,6 +44,7 @@ import numpy
 
 import theano
 import theano.tensor as T
+from theano.gof import graph
 
 
 class LogisticRegression(object):
@@ -415,6 +416,10 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
                         )
                     )
 
+                    # save the best model
+                    with open('best_model.pkl', 'w') as f:
+                        cPickle.dump(classifier, f)
+
             if patience <= iter:
                 done_looping = True
                 break
@@ -432,6 +437,38 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
     print >> sys.stderr, ('The code for file ' +
                           os.path.split(__file__)[1] +
                           ' ran for %.1fs' % ((end_time - start_time)))
+
+
+def predict():
+    """
+    An example of how to load a train model and use it
+    to predict labels.
+    """
+
+    # load the saved model
+    classifier = cPickle.load(open('best_model.pkl'))
+    y_pred = classifier.y_pred
+
+    # find the input to theano graph
+    inputs = graph.inputs([y_pred])
+    # select only x
+    inputs = [item for item in inputs if item.name == 'x']
+
+    # compile a predictor function
+    predict_model = theano.function(
+        inputs=inputs,
+        outputs=y_pred)
+
+    # We can test it on some examples from test test
+    dataset='mnist.pkl.gz'
+    datasets = load_data(dataset)
+    test_set_x, test_set_y = datasets[2]
+    test_set_x = test_set_x.get_value()
+
+    predicted_values = predict_model(test_set_x[:10])
+    print ("Predicted values for the first 10 examples in test set:")
+    print predicted_values
+
 
 if __name__ == '__main__':
     sgd_optimization_mnist()
