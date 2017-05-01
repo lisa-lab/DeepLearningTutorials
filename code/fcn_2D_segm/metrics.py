1766b4e777bb1f56909e6dba05ab52f8a4773bb8
StephanieLarocque <stephanie.larocque@umontreal.ca>
2017-05-01 17:13:24 -0400

    remove file

--- code/fcn_2D_segm/metrics.py
+++ code/fcn_2D_segm/metrics.py
@@ -2,13 +2,13 @@ import theano.tensor as T
 import numpy as np
 from theano import config
 
-from lasagne.objectives import squared_error as squared_error_lasagne
+
 
 _FLOATX = config.floatX
 _EPSILON = 10e-8
 
 
-def jaccard(y_pred, y_true, n_classes, one_hot=False):
+def jaccard_metric(y_pred, y_true, n_classes, one_hot=False):
 
     assert (y_pred.ndim == 2) or (y_pred.ndim == 1)
 
@@ -37,7 +37,7 @@ def jaccard(y_pred, y_true, n_classes, one_hot=False):
     return T.stack([num, denom], axis=0)
 
 
-def accuracy(y_pred, y_true, void_labels, one_hot=False):
+def accuracy_metric(y_pred, y_true, void_labels, one_hot=False):
 
     assert (y_pred.ndim == 2) or (y_pred.ndim == 1)
 
@@ -65,7 +65,7 @@ def accuracy(y_pred, y_true, void_labels, one_hot=False):
     return acc
 
 
-def crossentropy(y_pred, y_true, void_labels, one_hot=False):
+def crossentropy_metric(y_pred, y_true, void_labels, one_hot=False):
     # Clip predictions
     y_pred = T.clip(y_pred, _EPSILON, 1.0 - _EPSILON)
 
@@ -87,49 +87,5 @@ def crossentropy(y_pred, y_true, void_labels, one_hot=False):
     # Compute masked mean loss
     loss *= mask
     loss = T.sum(loss) / T.sum(mask)
-
-    return loss
-
-
-def binary_crossentropy(y_pred, y_true):
-    # Clip predictions to avoid numerical instability
-    y_pred = T.clip(y_pred, _EPSILON, 1.0 - _EPSILON)
-
-    loss = T.nnet.binary_crossentropy(y_pred, y_true)
-
-    return loss.mean()
-
-
-def entropy(y_pred):
-    # Clip predictions to avoid numerical instability
-    y_pred = T.clip(y_pred, _EPSILON, 1.0 - _EPSILON)
-
-    ent = - T.sum(y_pred * T.log(y_pred), axis=1)
-
-    return ent.mean()
-
-
-def squared_error_h(y_pred, y_true):
-
-    coef = np.linspace(_EPSILON, 1, len(y_pred)+1)[:-1]
-
-    error_list = [((a_i - b_i)**2).mean() for
-                  a_i, b_i in zip(y_pred, y_true)]
-    error_list = error_list * coef
-
-    return sum(error_list)
-
-
-def squared_error(y_pred, y_true, void):
-
-    if isinstance(void, int):
-        loss_aux = squared_error_lasagne(y_pred, y_true[:, :void, :, :]).mean(axis=1)
-        mask = y_true[:, :void, :, :].sum(axis=1)
-    else:  # assumes b,c,0,1
-        loss_aux = squared_error_lasagne(y_pred, y_true).mean(axis=1)
-        mask = T.neq(y_true.sum(1), sum(void))
-
-    loss_aux = loss_aux * mask
-    loss = loss_aux.sum()/mask.sum()
 
     return loss
